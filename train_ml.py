6fd9de6a815c0d5a223abe2e7d98f17722f37126
Nguyen Ba Thanh <thanhbait2k5@gmail.com>
2025-09-06 12:40:44 +0700

    second commit

--- train_ml.py
+++ train_ml.py
@@ -68,26 +68,7 @@ print(check)
 
 
 
-# # spliting
-# x=data[features]
-# print(x.head())
-# y=data.iloc[:,-1]
-# print(y.head())
 
-# # encoding
-# from sklearn.model_selection import train_test_split
-# x_train,x_valid,y_train,y_valid=train_test_split(x,y,test_size=0.2,random_state=42)
-# print(x_train.head())
-
-# # training ml
-# from sklearn.tree import DecisionTreeRegressor
-# dt_molde=DecisionTreeRegressor(random_state=42)
-# dt_molde.fit(x_train,y_train)
-# y_predict=dt_molde.predict(x_valid.head())
-# print(y_predict)
-# check=pd.DataFrame({"y":y_valid.head(),
-#                     "y_predict":y_predict})
-# print(check)
 
 
 
